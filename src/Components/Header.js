9f3a77ef3845147e0bae694aebf2ec27e436e6c9
Jahainzaib <jahainzaib.infobits@gmail.com>
2025-05-04 06:26:34 +0500

    changed name

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,17 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="container-fluid bg-light my-6 mt-0" id="home">
-        <div className="container">
-            <div className="row g-5 align-items-center">
-                <div className="col-lg-6 py-6 pb-0 pt-lg-0">
-                    <h3 className="text-primary mb-3">I'm</h3>
-                    <h1 className="display-3 mb-3">Syed Jahanzaib</h1>
-                    <h2 className="typed-text-output d-inline"></h2>
-                    <div className="typed-text d-none">Web Designer, Web Developer, Front End Developer, Apps Designer, Apps
-                        Developer</div>
-                    <div className="d-flex align-items-center pt-5">
+      <div className="container">
+        <div className="row g-5 align-items-center">
+          <div className="col-lg-6 py-4 pb-0 pt-lg-0" style={{ minHeight: "50vh" }}>
+            <h3 className="text-primary mb-3">We are</h3>
+            <h1 className="display-3 mb-3">TechNeko</h1>
+            <h2 className="typed-text-output d-inline"></h2>
+            <div className="typed-text d-none">
+              Web Designer, Web Developer, Front End Developer, Apps Designer,
+              Apps Developer
+            </div>
+            {/* <div className="d-flex align-items-center pt-5">
                         <a href="" className="btn btn-primary py-3 px-4 me-5">Download CV</a>
                         <button type="button" className="btn-play" data-bs-toggle="modal"
                             data-src="https://www.youtube.com/embed/DWRcNpR6Kdc" data-bs-target="#videoModal">
@@ -28,13 +30,13 @@ export default function Header() {
                             <a className="btn btn-square btn-primary me-2" target="_blank"
                                 href="https://github.com/Jahainzaib"><i className="fab fa-github"></i></a>
                         </div>
-                    </div>
-                </div>
-                <div className="col-lg-6">
-                    <img className="img-fluid" src="" alt=""/>
-                </div>
-            </div>
+                    </div> */}
+          </div>
+          <div className="col-lg-6">
+            <img className="img-fluid" src="" alt="" />
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
